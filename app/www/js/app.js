5a2096b062d83acaba843282153df7d4667aaa3e
spgitmonish <443615@COL-U443615L003.ad.seagate.com>
2016-01-10 21:40:50 -0700

    Demo ready!
    Ignore the other files

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -23,28 +23,149 @@ angular.module('starter', ['ionic', 'ngCordovaBeacon'])
 })
 
 .controller("ExampleController", function($scope, $rootScope, $ionicPlatform, $cordovaBeacon) {
-    $scope.beacons = {};
+    // Always hide the card(unless in beacon range)
+    $scope.showCard = false;
+    $scope.showBG = true;
 
-    // The plugin uses native device code, so any calls to it needs to be
-    // wrapped around $ionicPlatform.ready()
-    $ionicPlatform.ready(function() {
+    // Add device ready event listener
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-        // This is requires for iOS only, this statement is void for Android
-        $cordovaBeacon.requestWhenInUseAuthorization();
+    // Callback to determing state for the region
+    function didDetermineStateForRegion(pluginResult) {
+        //console.log(JSON.stringify(pluginResult));
+    }
 
-        // Check if any beacons are in range and how far it is
-        $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function(event, pluginResult) {
-            var uniqueBeaconKey;
-            for(var i = 0; i < pluginResult.beacons.length; i++) {
-                uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
-                $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
+    // Callback to start monitoring a region
+    function didStartMonitoringForRegion (pluginResult) {
+        //console.log(JSON.stringify(pluginResult));
+    }
+
+    // Callback called when exiting a region
+    function didExitRegion(pluginResult) {
+        /*$cordovaLocalNotification.add({
+        id: 30244234234,
+        title: "Good By!",
+        text: "Hope to see you again."
+            }).then(function () {
+            });*/
+    }
+
+    // Callback called when entering a region
+    function didEnterRegion (pluginResult) {
+        /*$cordovaLocalNotification.add({
+        title: "Welcome"
+        text: "Tap to launch app"
+            }).then(function () {
+
+            });*/
+    }
+
+    // Callback called if Ranging is enabled
+    function didRangeBeaconsInRegion (pluginResult) {
+        // Capture the closest beacon regardless of the length
+        var closestBeacon = pluginResult.beacons[0];
+
+        if(closestBeacon){
+            if(closestBeacon["accuracy"] <= 1.4){
+                // Set ng-show to true to show the card as we are in range
+                $scope.showCard = true;
+                $scope.showBG = false;
+
+                // This is required for binding the data to the HTML element
+                // Also to notify that there has been a change in the data.
+                $scope.$apply();
+
+                if(closestBeacon["uuid"] == "31b32e3c-5e8b-68f8-0493-ff9dcaa7c4b7"){
+                    console.log("Show Mint: " + closestBeacon["accuracy"]);
+
+                    document.getElementById("cardheader").innerHTML = "Welcome to Aikyatha!";
+                    document.getElementById("cardimage").src = "img/Kanji1.jpg"
+                    document.getElementById("cardfooter").innerHTML = "Explore Kanjivarams and more...";
+                }
+
+                if(closestBeacon["uuid"] == '29644b63-3ebd-ae99-ed00-0608f6a708a4'){
+                    console.log("Show Ice: " + closestBeacon["accuracy"]);
+                    document.getElementById("cardheader").innerHTML = "Exclusive Design";
+                    document.getElementById("cardimage").src = "img/Kanji2.jpg";
+                    document.getElementById("cardfooter").innerHTML = "Great Styles & Colors!";
+                }
+
+                if(closestBeacon["uuid"] == '70e09360-015d-dc97-ccd3-f1593b8a4297'){
+                    console.log("Show Blueberry: " + closestBeacon["accuracy"]);
+                    document.getElementById("cardheader").innerHTML = "Limited time offer";
+                    document.getElementById("cardimage").src = "img/sample.png";
+                    document.getElementById("cardfooter").innerHTML = "Show code for 20% off!";
+                }
             }
-            // This is required for the binding the data to the HTML element
-            $scope.$apply();
-        });
+        }
+    }
 
-        // Create a set of beacons to range(using the factory set UUID)
-        $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("estimote", "b9407f30-f5f8-466e-aff9-25556b57fe6d"));
+    // Wait for the device to get ready
+    function onDeviceReady() {
+        var delegate = new cordova.plugins.locationManager.Delegate();
 
-    });
+        delegate.didDetermineStateForRegion = didDetermineStateForRegion;
+        delegate.didStartMonitoringForRegion = didStartMonitoringForRegion;
+        delegate.didRangeBeaconsInRegion = didRangeBeaconsInRegion;
+        delegate.didEnterRegion = didEnterRegion;
+        delegate.didExitRegion = didExitRegion;
+
+        cordova.plugins.locationManager.setDelegate(delegate);
+
+        // Required only for iOS 8+
+        cordova.plugins.locationManager.requestAlwaysAuthorization();
+
+        // Create the beacon regions(with ranging and monitoring)
+        createBeacons();
+
+        // Function to create multiple beacons(customize this for own use)
+        // Start both Ranging and Monitoring
+        function createBeacons(){
+            var mintBeaconRegion = createBeacon('mintEstimote', '31B32E3C-5E8B-68F8-0493-FF9DCAA7C4B7', 10002, 48896);
+            var iceBeaconRegion = createBeacon('iceEstimote', '29644B63-3EBD-AE99-ED00-0608F6A708A4', 28374, 41843);
+            var blueberryBeaconRegion = createBeacon('blueberryEstimote', '70E09360-015D-DC97-CCD3-F1593B8A4297', 56444, 40894);
+
+            // Start monitoring all the beacons
+            // NOTE: Advantages: 1. Works in the foreground, background, and even when the app is killed.
+            //                   2. Can detect entering and exiting a region
+            //    Disadvantages: 1. Not as responsive as ranging(as it uses low power scanning)
+            //                   2. If Wi-Fi/Bluetooth is in use or used recently, the response
+            //                      is instantaneous but slow otherwise
+            /*cordova.plugins.locationManager.startMonitoringForRegion(mintBeaconRegion)
+                .fail(console.error)
+                .done();
+
+            cordova.plugins.locationManager.startMonitoringForRegion(iceBeaconRegion)
+                .fail(console.error)
+                .done();
+
+            cordova.plugins.locationManager.startMonitoringForRegion(blueberryBeaconRegion)
+                .fail(console.error)
+                .done();*/
+
+            // Start ranging all the beacons(not used for now, see Disadvantages)
+            // NOTE: Advantages: More responsive compared to monitoring
+            //    Disadvantages: 1. Not advisable to run in background(Apple will likely reject it)
+            //                   2. When running in the background can drain the battery
+            cordova.plugins.locationManager.startRangingBeaconsInRegion(mintBeaconRegion)
+                .fail(console.error)
+                .done();
+
+            cordova.plugins.locationManager.startRangingBeaconsInRegion(iceBeaconRegion)
+                .fail(console.error)
+                .done();
+
+            cordova.plugins.locationManager.startRangingBeaconsInRegion(blueberryBeaconRegion)
+                .fail(console.error)
+                .done();
+        }
+
+        // Function to create a beacon region
+        function createBeacon(identifier, uuid, major, minor) {
+            // throws an error if the parameters are not valid
+            var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+            return beaconRegion;
+        }
+
+    }
 });
